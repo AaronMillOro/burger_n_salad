5fec298d05a668e44d137d8fdda686890c00a77c
Aaron MillOro <7230016+AaronMillOro@users.noreply.github.com>
2024-05-19 04:06:14 +0200

    :alembic: start of project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+function navContent() {
+    const elem = document.createElement('div')
+    elem.innerText = 'Start of project skeleton'
+    return elem
+}
+
+document.body.appendChild(navContent())
