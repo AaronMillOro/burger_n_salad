7f44621b7f30677608360fae2ca04e650b873419
Aaron MillOro <7230016+AaronMillOro@users.noreply.github.com>
2024-06-03 16:23:19 +0200

    :hammer: base layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
+import './style.css'
+import Logo from './assets/img/cutlery.png'
 
-function navContent() {
-    const elem = document.createElement('div')
-    elem.innerText = 'Start of project skeleton'
-    return elem
+const header = document.querySelector('header')
+const content = document.getElementById('content')
+
+function displayLogo(){
+    const logo = new Image()
+    logo.src = Logo
+    logo.setAttribute('alt', 'logo')
+    logo.setAttribute('id', 'logo')
+    return logo
 }
 
-document.body.appendChild(navContent())
+function showNavContent() {
+    const nav = document.querySelector('nav')
+    const about = document.createElement('button')
+    const menu = document.createElement('button')
+    const contact = document.createElement('button')
+    about.innerText = 'About'
+    menu.innerText = 'Menu'
+    contact.innerText = 'Contact'
+    about.classList.add('btn')
+    about.classList.add('about')
+    menu.classList.add('btn')
+    menu.classList.add('menu')
+    contact.classList.add('btn')
+    contact.classList.add('contact')
+    nav.appendChild(about)
+    nav.appendChild(menu)
+    nav.appendChild(contact)
+    return nav
+}
+
+header.appendChild(displayLogo())    
+header.appendChild(showNavContent())    
+
