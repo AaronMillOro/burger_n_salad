6e88a6c118c0addfa62f5a87a8c129e1dfd755fc
Aaron MillOro <7230016+AaronMillOro@users.noreply.github.com>
2024-06-05 02:13:56 +0200

    :recycle: refactor getImage function

--- src/about.js
+++ src/about.js
@@ -1,33 +1,21 @@
 import Burger from './assets/img/food-photographer-E94j3rMcxlw-unsplash.jpg'
 import Salad from './assets/img/anna-pelzer-IGfIGP5ONV0-unsplash.jpg'
+import { getImage } from './menu.js'
 
-const burgerImage = () => {
-    const burger = new Image()
-    burger.src = Burger
-    burger.setAttribute('alt', 'main-burger')
-    burger.classList.add('main-food')
-    return burger
-}
-
-const saladImage = () => {
-    const salad = new Image()
-    salad.src = Salad
-    salad.setAttribute('alt', 'main-salad')
-    salad.classList.add('main-food')
-    return salad
-}
 
 const showAbout = () => {
     const aboutSection = showIndex()
     const contentSection = document.createElement('section')
     contentSection.classList.add('info')
     const info = document.createElement('p')
-    contentSection.appendChild(burgerImage())
+    const burger = getImage(Burger, 'main burger')
+    contentSection.appendChild(burger)
     info.innerText = `
     Grab some burgers and salads. It's totally okay to indulge in some comfort food (miam burgers). But don't forget to balance it out with some healthy options like salad. And don't forget about the little joys in life, like ponies or sweets. 
     `
     contentSection.appendChild(info)
-    contentSection.appendChild(saladImage())
+    const salad = getImage(Salad, 'main salad')
+    contentSection.appendChild(salad)
     aboutSection.appendChild(contentSection)
     return aboutSection
 }
--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,32 @@
+import Cheese from './assets/img/aliona-gumeniuk-jeAjT87nbjM-unsplash.jpg'
+import Bread from './assets/img/anna-monina-rafal-vX075UlhgP4-unsplash.jpg'
+import Burger from './assets/img/delfina-cocciardi-NDPBK-d-03M-unsplash.jpg'
+import Salad from './assets/img/anna-pelzer-IGfIGP5ONV0-unsplash.jpg'
+import Natas from './assets/img/samantha-gollnick-cnr5T5HUiZE-unsplash.jpg'
+import Panacota from './assets/img/wesual-click-BgirKolxbIk-unsplash.jpg'
+
+
+const getImage = (importedImage, description) => {
+    const img = new Image()
+    img.src = importedImage
+    img.setAttribute('alt', description)
+    img.classList.add('main-food')
+    return img
+}
+
+
+//const cheese = getImage(Cheese, 'cantal entre-deux')
+
 const showMenu = () => {
-    return alert('Menu')
+    const menuSection = document.createElement('section')
+    const title = document.createElement('h1')
+    title.innerText = 'Delicious deals'
+    menuSection.appendChild(title)
+    return menuSection
 }
 
 
 export {
+    getImage,
     showMenu
 }
