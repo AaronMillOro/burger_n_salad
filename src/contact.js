514610e5b2326e4f470a4a7e8f3b883f6768a7c3
Aaron MillOro <7230016+AaronMillOro@users.noreply.github.com>
2024-06-05 03:18:43 +0200

    :hammer: contact page skeleton

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,16 @@
 const showContact = () => {
-    return alert('Contact')
+    const contactSection = document.createElement('section')
+    contactSection.classList.add('page-details')
+    const title = document.createElement('h1')
+    title.innerText = 'Where to find us ?'
+    const info = document.createElement('p')
+    info.innerText = `
+    From Monday to Saturday
+    From 11 a.m until 11 p.m
+    We are here 👨‍🍳`
+    contactSection.appendChild(title)
+    contactSection.appendChild(info)
+    return contactSection
 }
 
 
